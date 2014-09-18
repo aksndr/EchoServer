6dadcab588f4c5d5b01bb024e2b1f1b789c88af6
aksndr <aksndr@gmail.com>
2014-09-18 22:06:15 +0400

    Initial commit

---
+++ src/main/java/ru/aksndr/EchoServer.java
@@ -0,0 +1,49 @@
+package ru.aksndr;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.*;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by aksndr on 18.09.2014.
+ */
+public class EchoServer  {
+
+    public static void main(String[] args) throws Exception {
+        ChannelFactory factory =
+                new NioServerSocketChannelFactory(
+                        Executors.newCachedThreadPool(),
+                        Executors.newCachedThreadPool());
+
+        ServerBootstrap bootstrap = new ServerBootstrap(factory);
+
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            public ChannelPipeline getPipeline() {
+                return Channels.pipeline(new EchoServerHandler());
+            }
+        });
+
+        bootstrap.setOption("child.tcpNoDelay", true);
+        bootstrap.setOption("child.keepAlive", true);
+
+        bootstrap.bind(new InetSocketAddress(8080));
+    }
+
+
+
+
+    public static class EchoServerHandler extends SimpleChannelUpstreamHandler{
+        public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+            ChannelBuffer buf = (ChannelBuffer) e.getMessage();
+            while(buf.readable()) {
+                System.out.println((char) buf.readByte());
+                System.out.flush();
+            }
+        }
+    }
+
+}
